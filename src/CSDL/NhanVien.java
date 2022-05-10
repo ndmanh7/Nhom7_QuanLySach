ac10df44a032e270aab8bf24aca55c44dd6c1a0b
manh <ndmanh10a7@gmail.com>
2022-05-10 21:59:00 +0700

    update Sach va NhanVien

--- src/CSDL/NhanVien.java
+++ src/CSDL/NhanVien.java
@@ -9,5 +9,52 @@ package CSDL;
  * @author ADMIN
  */
 public class NhanVien {
+    private String maNhanVien;
+    private String tenNhanVien;
+    private String ngayVaoLam;
+    private int soNgayNghi;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String maNhanVien, String tenNhanVien, String ngayVaoLam, int soNgayNghi) {
+        this.maNhanVien = maNhanVien;
+        this.tenNhanVien = tenNhanVien;
+        this.ngayVaoLam = ngayVaoLam;
+        this.soNgayNghi = soNgayNghi;
+    }
+
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getTenNhanVien() {
+        return tenNhanVien;
+    }
+
+    public void setTenNhanVien(String tenNhanVien) {
+        this.tenNhanVien = tenNhanVien;
+    }
+
+    public String getNgayVaoLam() {
+        return ngayVaoLam;
+    }
+
+    public void setNgayVaoLam(String ngayVaoLam) {
+        this.ngayVaoLam = ngayVaoLam;
+    }
+
+    public int getSoNgayNghi() {
+        return soNgayNghi;
+    }
+
+    public void setSoNgayNghi(int soNgayNghi) {
+        this.soNgayNghi = soNgayNghi;
+    }
+    
     
 }
--- src/CSDL/Sach.java
+++ src/CSDL/Sach.java
@@ -9,5 +9,70 @@ package CSDL;
  * @author ADMIN
  */
 public class Sach {
+    private String maSach;
+    private String tenSach;
+    private String nhaXuatBan;
+    private String tacGia;
+    private int soLuongCon;
+    private String tinhTrang;
+
+    public Sach() {
+    }
+
+    public int getSoLuongCon() {
+        return soLuongCon;
+    }
+
+    public void setSoLuongCon(int soLuongCon) {
+        this.soLuongCon = soLuongCon;
+    }
+
+    public String getTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(String tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+
+    public Sach(String maSach, String tenSach, String nhaXuatBan, String tacGia) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.nhaXuatBan = nhaXuatBan;
+        this.tacGia = tacGia;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getNhaXuatBan() {
+        return nhaXuatBan;
+    }
+
+    public void setNhaXuatBan(String nhaXuatBan) {
+        this.nhaXuatBan = nhaXuatBan;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+    
     
 }
